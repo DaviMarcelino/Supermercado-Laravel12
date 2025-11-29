66b3641f06ab108370ab76ea71620992164ac8f2
Davi Marcelino <marcelinodavi265@gmail.com>
2025-11-29 09:20:55 -0300

    commit

--- supermercado-main/routes/web.php
+++ supermercado-main/routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/', function () {
-    return redirect('/productos');
+    return redirect('/login');
 });
 
 Route::get('/dashboard', function () {
